b54a1527f911abc5782fce2a0b88bcd395f78e1d
huy37204 <huy37204@gmail.com>
2025-05-08 15:01:59 +0700

    edit vite.config.ts to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Cho phép truy cập từ bên ngoài
+    port: 8082, // Port chạy trên Droplet
+    strictPort: true, // Không tự động thay đổi port nếu bị chiếm
+    watch: {
+      usePolling: true, // Fix hot reload khi chạy trên Docker hoặc Droplet
+    },
     proxy: {
       "/socket.io": {
         target: "http://localhost:3000",
         ws: true,
+        changeOrigin: true, // Thay đổi origin header
       },
+    },
+    hmr: {
+      host: "http://huytran3.workspace.opstech.org/", // Thay bằng IP của Droplet
+      port: 8082,
     },
   },
 });
